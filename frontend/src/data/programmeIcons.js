bc6553555e7ad7c5ccb251ec5e64e9a58f3c502e
geo-web-maker <geo_web@yahoo.com>
2026-08-20 10:51:52 +0300

    Delete frontend directory

--- frontend/src/data/programmeIcons.js
+++
@@ -1,4 +0,0 @@
-import { Search, Droplet, ShieldCheck, HardHat, Wheat, HeartPulse, Stethoscope, Handshake } from 'lucide-react';
-
-export const PROGRAMME_ICONS = { Search, Droplet, ShieldCheck, HardHat, Wheat, HeartPulse, Stethoscope, Handshake };
-export const PROGRAMME_ICON_NAMES = Object.keys(PROGRAMME_ICONS);
